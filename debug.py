c6693f9212fd38fa69c5a87acd8d613b0c1adb1a
Ahmed <65268218+AhmedAlkhafai99@users.noreply.github.com>
2020-05-30 16:50:09 -0700

    Last update 5/5 1650

--- debug.py
+++ debug.py
@@ -12,14 +12,14 @@ def lone_sum(a, b, c):
 
 ----------
 -------
-scenario 1:
-Let us assume a=12,b=9 c=4
-if a>=b true so we will return c which is 4
-
-scenario 2:
-Let us assume a=8,b=9 c=4
-if a>=b false
-a==c false
-b==c false
-a==b and a==c and b==c false
-so we will return a+b+c which is 21
+def lone_sum(a, b, c):
+    if a == b and a == c and b == c:
+        return c
+    elif a >= b:
+        return b
+    elif b == c:
+        return a
+    elif a == c:
+        return b
+    else:
+        print(lone_sum(3, 5, 7)
